daed7ce841f1db80b4443dae48082d63e82ce11a
Vijay <knvijay2308@gmail.com>
2026-07-20 21:17:15 +0530

    Added AI report PDF download feature

--- genai/app.py
+++ genai/app.py
@@ -1,8 +1,31 @@
+from reportlab.lib.pagesizes import letter
+from reportlab.pdfgen import canvas
+import tempfile
 import streamlit as st
 from pypdf import PdfReader
 from bedrocks import analyze_claim
 from prompts import CLAIM_ANALYSIS_PROMPT
+def generate_pdf(report):
 
+    temp_file = tempfile.NamedTemporaryFile(delete=False, suffix=".pdf")
+
+    c = canvas.Canvas(temp_file.name, pagesize=letter)
+
+    y = 750
+
+    for line in report.split("\n"):
+
+        c.drawString(40, y, line[:100])
+
+        y -= 20
+
+        if y < 40:
+            c.showPage()
+            y = 750
+
+    c.save()
+
+    return temp_file.name
 st.set_page_config(
     page_title="ClaimsIQ - AI Claim Analyzer",
     page_icon="📄",
@@ -45,6 +68,15 @@ if uploaded_file is not None:
 
         st.subheader("AI Claim Analysis")
         st.write(report)
+        pdf_file = generate_pdf(report)
 
+        with open(pdf_file, "rb") as file:
+
+            st.download_button(
+        label="📥 Download AI Report",
+        data=file,
+        file_name="ClaimsIQ_AI_Report.pdf",
+        mime="application/pdf"
+    )
     else:
         st.warning("No text found in the uploaded PDF.")
